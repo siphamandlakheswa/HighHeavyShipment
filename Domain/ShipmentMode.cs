746b08b1aa2867b3dac3837e6196dd0e8e8da05b
Siphamandla Pacificus Deusdedit Kheswa <siphamandlakheswa@outlook.com>
2026-02-11 23:07:07 +0200

    Create ShipmentMode.cs file with shipment mode enum

---
+++ Domain/ShipmentMode.cs
@@ -0,0 +1,9 @@
+namespace HighHeavyShipment.Domain;
+/// <summary>
+/// Transportation mode enum for shipments.
+/// </summary>
+public enum ShipmentMode {
+    Sea = 0,
+    Land = 1,
+    Air = 2
+}
