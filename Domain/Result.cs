2a45ca9616bad2dbf0444a1d94031533d344dfec
Siphamandla Pacificus Deusdedit Kheswa <siphamandlakheswa@outlook.com>
2026-02-11 23:08:35 +0200

    Create Result.cs for Railway-Oriented Programming

---
+++ Domain/Result.cs
@@ -0,0 +1,38 @@
+// Result.cs
+
+using System;
+
+namespace YourNamespace
+{
+    public class Result
+    {
+        public bool IsSuccess { get; private set; }
+        public string Error { get; private set; }
+
+        protected Result(bool isSuccess, string error)
+        {
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result Success() => new Result(true, null);
+        public static Result Failure(string error) => new Result(false, error);
+    }
+
+    public class Result<T>
+    {
+        public T Value { get; private set; }
+        public bool IsSuccess { get; private set; }
+        public string Error { get; private set; }
+
+        protected Result(bool isSuccess, T value, string error)
+        {
+            IsSuccess = isSuccess;
+            Value = value;
+            Error = error;
+        }
+
+        public static Result<T> Success(T value) => new Result<T>(true, value, null);
+        public static Result<T> Failure(string error) => new Result<T>(false, default, error);
+    }
+}
